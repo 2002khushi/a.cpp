71ab74fb7a3cedd65cca63ece6fc876bb6bdbca7
khushi chaudhary <142868729+2002khushi@users.noreply.github.com>
2024-04-14 02:04:35 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5]={10,15,30,50,56};
+    cout <<"array elements:";
+    for(int i = 0; i<5; ++i){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
